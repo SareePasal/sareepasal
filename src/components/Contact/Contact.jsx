198f2675153a18e7ece85b4eed35d10cca6bf603
Deepak <dadhikari@my.ccri.edu>
2024-09-29 16:53:09 -0400

    Added Contact

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import About from "../../components/Contact/Contact"
+import Footer from "../../components/Footer/Footer"
+
+export default function Ab() {
+  return (
+    <main class="flex flex-col h-dvh ">
+      <Header/>
+      <Contact/>
+      <Footer/>
+    </main>
+  );
+}
