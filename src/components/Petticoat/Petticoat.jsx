136b00185782df39d8c5149fcb1cc194ba1401ed
Saree Pasal <dadhikari@my.ccri.edu>
2024-10-05 12:57:09 -0400

    Petticoat Added

--- src/components/Petticoat/Petticoat.jsx
+++ src/components/Petticoat/Petticoat.jsx
@@ -1,13 +1,104 @@
 import * as React from 'react';
+import Image from 'next/image'
 
 const Petticoat = () => {
-    return(
-<div class="h-svh  bg-white flex">
-            <div class="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
-            <span class="text-2xl font-extrabold md:text-lg font-semibold whitespace-nowrap text-red-500 dark:text-gray-400">
-                    This Page is Under Construction !!!!!</span>
-            </div>
-        </div>    )
+    return (
+        <div class="grid lg:grid-cols-3 lg:gap-36 sm:grid-cols-2 gap-20 ">
+            < Petticoats key="1" />
+        </div>
+
+    )
 }
 
-export default Petticoat
+const Petticoats = () => {
+    return(
+        <React.Fragment>
+            {/* Vendor: Leranath Fashiom Item: IC-2411*/}
+            <div class="aspect-w-16 aspect-h-16 flex-row">
+                <div className="flex md:item-center md:justify-center md:w-11/12 ">
+                    <div class="relative rounded-xl overflow-auto w-11/12">
+                        <div class="relative w-full flex md:gap-6 gap-3 snap-x overflow-x-auto pb-14 ">
+                            <div class=" snap-center shrink-0 mx-7 md:flex hidden">
+                                <div class="shrink-0 w-96"></div>
+                            </div>
+                            <div class="snap-normal snap-center shrink-0 shadow-2xl rounded-2xl  md:mx-7 bg-white">
+                                <Image class="rounded-2xl " src="/Images/Petticoat/Leranath Fashion/Petticoat1.jpg" alt="1" width={300} height={300} />
+                            </div>
+                            <div class="snap-center snap-always shrink-0 md:mx-7  scroll-ms-1 md:scroll-ms-6 shadow-xl rounded-2xl">
+                                <Image class="rounded-2xl" src="/Images/Petticoat/Leranath Fashion/Petticoat2.jpg" alt="2" width={300} height={300} />
+                            </div>
+                            <div class="snap-center snap-always shrink-0 md:mx-7 scroll-ms-1 md:scroll-ms-6 shadow-lg rounded-2xl">
+                                <Image class="rounded-2xl" src="/Images/Petticoat/Leranath Fashion/Petticoat3.jpg" alt="3" width={300} height={300} />
+                            </div>
+                            <div class="snap-center snap-always shrink-0 md:mx-7 scroll-ms-1 md:scroll-ms-6 shadow-lg rounded-2xl">
+                                <Image class="rounded-2xl" src="/Images/Petticoat/Leranath Fashion/Petticoat4" alt="4" width={300} height={300} />
+                            </div>
+                            <div class="snap-center snap-always shrink-0 md:mx-7 scroll-ms-1 md:scroll-ms-6 shadow-lg rounded-2xl">
+                                <Image class="rounded-2xl" src="/Images/Petticoat/Leranath Fashion/Petticoat5.jpg" alt="5" width={300} height={300} />
+                            </div>
+                            <div class="snap-normal snap-center shrink-0 shadow-2xl rounded-2xl  md:mx-7 bg-white">
+                                <Image class="rounded-2xl " src="/Images/Petticoat/Leranath Fashion/Petticoat6.jpg" alt="6" width={300} height={300} />
+                            </div>
+                            <div class=" snap-center shrink-0 md:mx-7 md:flex hidden ">
+                                <div class="shrink-0 w-96"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-1 gap-4 relative flex justify-between  sm:w-5/6 h-full mx-auto ">
+                    <div>
+                        <h1 class=" mb-2 text-2xl md:text-2xl sm:font-extrabold tracking-tight leading-none 
+                                        text-blue-900 xl:text-2xldark:text-blue-950">
+                            Petticoat
+                        </h1>
+                        <p class="text-black text-smdark:text-blue-950 text-left">
+                            Product Details:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+
+                            </p>
+
+                            <br />Size & Fit:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+                                One Size Fits All
+                            </p>
+
+                            <br />Colors:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+                                Blue, Red, Black, Green, Rani, Maroon, Violet, Gold, Nude
+                            </p>
+                            <br />Material & Care:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+                                Dry Wash Only
+                            </p>
+
+                            <br />Product Code:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+                                
+                            </p>
+
+                            <br />Note:
+                            <p class="text-black text-smdark:text-blue-950 text-left font-light">
+                                Product color may slightly vary due to photographic lighting sources or your monitor settings.
+                            </p>
+
+                            <br />Price:
+                            <p class="text-green-700 font-bold text-3xl">
+                                $8.99
+                            </p>
+                        </p>
+                        <svg class="h-16 w-16 text-black" width="24" height="24" viewBox="0 0 24 24"
+                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                            <path d="M15 7a2 2 0 0 1 2 2" />
+                            <path d="M15 3a6 6 0 0 1 6 6" />
+                        </svg>
+                        <p>To Make an Order Call : (347)771-2375</p>
+                    </div>
+                </div>
+            </div>        
+            </React.Fragment>
+  )
+}
+
+export default Petticoats
