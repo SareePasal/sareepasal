72007134f688882a76a913796b293cfe31fe75d9
anityam <89035703+anityam@users.noreply.github.com>
2024-10-06 11:50:59 -0400

    phone info button fix

--- src/components/Saree/Sarees.jsx
+++ src/components/Saree/Sarees.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Image from 'next/image'
+import PhoneInfo from '../phone/PhoneInfo'
 
 const Saree = () => {
     return (
@@ -83,14 +84,7 @@ const Sarees = () => {
                                 $79.99
                             </p>
                         </p>
-                        <svg class="h-16 w-16 text-black" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />
-                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            <path d="M15 7a2 2 0 0 1 2 2" />
-                            <path d="M15 3a6 6 0 0 1 6 6" />
-                        </svg>
-                        <p>To Make an Order Call : (347)771-2375</p>
+                        <PhoneInfo/>
                     </div>
                 </div>
             </div>
@@ -174,14 +168,7 @@ const Sarees = () => {
                                 $79.99
                             </p>
                         </p>
-                        <svg class="h-16 w-16 text-black" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />
-                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            <path d="M15 7a2 2 0 0 1 2 2" />
-                            <path d="M15 3a6 6 0 0 1 6 6" />
-                        </svg>
-                        <p>To Make an Order Call : (347)771-2375</p>
+                        <PhoneInfo/>
                     </div>
                 </div>
             </div>
@@ -257,14 +244,7 @@ const Sarees = () => {
                                 $99.99
                             </p>
                         </p>
-                        <svg class="h-16 w-16 text-black" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />
-                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            <path d="M15 7a2 2 0 0 1 2 2" />
-                            <path d="M15 3a6 6 0 0 1 6 6" />
-                        </svg>
-                        <p>To Make an Order Call : (347)771-2375</p>
+                        <PhoneInfo/>
                     </div>
                 </div>
             </div>
@@ -340,14 +320,7 @@ const Sarees = () => {
                                 $79.99
                             </p>
                         </p>
-                        <svg class="h-16 w-16 text-black" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />
-                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            <path d="M15 7a2 2 0 0 1 2 2" />
-                            <path d="M15 3a6 6 0 0 1 6 6" />
-                        </svg>
-                        <p>To Make an Order Call : (347)771-2375</p>
+                        <PhoneInfo/>
                     </div>
                 </div>
             </div>
