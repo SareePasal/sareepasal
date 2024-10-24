39c0fece4b80acc7c0f701a0bfa3317c148f3d5a
anityam <89035703+anityam@users.noreply.github.com>
2024-10-24 11:54:43 -0400

    push uild

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,9 +11,9 @@ export const metadata = {
   description: "Saree Pasal",
 };
 
-export default function RootLayout({ children,pageProps }) {
+export default function RootLayout({ children }) {
   return (
-    <StoreProvider {...pageProps}>
+    <StoreProvider>
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
