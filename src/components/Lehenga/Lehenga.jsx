f8b3e8049c6e61a34a706a3ad4eb4e6594931d81
Saree Pasal <dadhikari@my.ccri.edu>
2024-11-09 18:31:12 -0500

    Updated Golden Lehenga

--- src/components/Lehenga/Lehenga.jsx
+++ src/components/Lehenga/Lehenga.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Slider from '../Slider/Slider'
 import Description from "../Description/Description"
-import L3074 from "./L3074"
-import L3057 from "./L3057"
 import L3066 from "./L3066"
+import L3057 from "./L3057"
+import L3074 from "./L3074"
+
 
 //Add Items Here
-const itemCode = [L3074,L3057,L3066]
+const itemCode = [L3066,,L3057,L3074]
 
 //Do Not Touch the code below
 const Lehengas = () => {
