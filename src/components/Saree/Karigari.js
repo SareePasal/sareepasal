f02639275a1fb739e00140dee38c0ca99ab568b5
LINS-NY <longislandnepalese@gmail.com>
2024-12-01 23:25:29 -0500

    Added More Products

---
+++ src/components/Saree/Karigari.js
@@ -0,0 +1,24 @@
+const item = {
+    images: [
+        { src: '/Images/RoyalTextile/Karigari/1 (2).jpeg', alt: '3', width: 430, height: 430 },
+        { src: '/Images/RoyalTextile/Karigari/1 (3).jpeg', alt: '2', width: 430, height: 430 },
+        { src: '/Images/RoyalTextile/Karigari/1 (4).jpeg', alt: '3', width: 430, height: 430 },
+        { src: '/Images/RoyalTextile/Karigari/1 (5).jpeg', alt: '1', width: 430, height: 430 },
+
+    ],
+    description: {
+        title: "Sethnic Karigari Chiffon Embroidered Light Weight Fancy Saree",
+        detail: ["Saree: Sethnic Karigari Chiffon Embroidered Light Weight Fancy Saree",
+            "Type: Embroidered Saree",
+            "Work : Saree - Cutdana Work, Stone Work, Blouse - Cutdana Work, Stone Work",
+            "Blouse: Cutdana Blouse",
+        ],
+        colors: "As shown in Picture",
+        size: "Call for Sizes",
+        code: "Karigari",
+        price: "$80.00"
+    }
+
+}
+
+export default item
